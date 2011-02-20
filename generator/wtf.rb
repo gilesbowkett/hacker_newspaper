9cce6dc54b99d304ff1b36addd45cad1f56aeb48
BHousel <bryan@7thposition.com>
2011-02-20 15:47:07 -0500

    replace Hpricot with Nokogiri use stringex to cleanup 'smart' quotes use feedzirra for feed parsing..

--- generator/wtf.rb
+++ generator/wtf.rb
@@ -1,26 +1,21 @@
 #!/usr/bin/ruby
-
-# something's wrong, I think it's that Hacker News sometimes goes down or takes too long to respond
+# encoding: utf-8
 
 exit if File.new("latest.rss").stat.zero?
 
 require 'rubygems'
-gem 'hpricot', '= 0.6'
-require 'feed-normalizer'
+gem 'nokogiri'
+gem 'stringex'
+gem 'feedzirra'
+
+require 'feedzirra'
+require 'stringex'
 require 'open-uri'
 require 'erb'
 
-# ActiveSupport has some dooky-ass issue with motherfucking Hpricot
 class String
   def blank?
     self !~ /\S/
-  end
-  def new_york_times_get_over_your_pretentious_bullshit_please
-    self.gsub!(/’/, "'")
-    self.gsub!(/“/, "\"")
-    self.gsub!(/”/, "\"")
-    self.gsub!(/—/, "-")
-    self.gsub!(/–/, "-")
   end
 end
 
@@ -46,17 +41,15 @@ end
 class Story < Struct.new(:headline, :domain, :url, :text) ; end
 
 File.open(File.expand_path(File.dirname(__FILE__)) + "/../public/index.html", "w") do |file|
-  feed = FeedNormalizer::FeedNormalizer.parse(File.read(File.expand_path(File.dirname(__FILE__)) + "/latest.rss"))
-  feed.clean!
+  feed = Feedzirra::Feed.parse(File.read(File.expand_path(File.dirname(__FILE__)) + "/latest.rss"))
+  feed.sanitize_entries!
 
   @stories = (feed.entries.collect do |entry|
     next unless entry.url && entry.title
 
-    title = entry.title
-    text = Hpricot(entry.content).to_plain_text
-    [text, title].each do |string|
-      string.new_york_times_get_over_your_pretentious_bullshit_please
-    end
+    title = entry.title.remove_formatting
+    text = Nokogiri::XML(entry.content).to_s.remove_formatting
+
     text.gsub!(/^Comments \[http:\/\/news\.ycombinator\.com\/item\?id=\d+\]/, "")
     text.gsub!(/\[[^\[]+\]/, "")
     text = text[0..300]
