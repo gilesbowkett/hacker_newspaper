3663e42341f92b494cd7c052219e5744516164ed
Giles Bowkett <gilesb@gmail.com>
2011-07-20 14:10:55 -0700

    manual runner for the process

---
+++ wtf.bash
@@ -0,0 +1,3 @@
+# manually update site
+
+python /webapps/hacker_newspaper/generator/hacker_news_deluxe_rss.py > /webapps/hacker_newspaper/generator/latest.rss ; ruby /webapps/hacker_newspaper/generator/wtf.rb 
