362b2bf5da270aefbe8be4a0a957257df8eed340
Giles Bowkett <gilesb@gmail.com>
2011-08-13 16:47:11 -0700

    variable names, method names, readability

--- generator/wtf.rb
+++ generator/wtf.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/ruby
 
-def booty_gremlins
-  # TODO: rename this something like "set_up_error_page"
-  system("cp /webapps/hacker_newspaper/public/booty_gremlins.html /webapps/hacker_newspaper/public/index.html")
+def error_page
+  dir = "/webapps/hacker_newspaper/public"
+  system("cp #{dir}/booty_gremlins.html #{dir}/index.html")
   exit
 end
 
 # Hacker News sometimes goes down or takes too long to respond, so, bail if that happens
-booty_gremlins if File.new(File.expand_path(File.dirname(__FILE__)) + "/latest.rss").stat.zero?
+error_page if File.new(File.expand_path(File.dirname(__FILE__)) + "/latest.rss").stat.zero?
 
 require 'rubygems'
 gem 'hpricot', '= 0.6'
@@ -53,7 +53,8 @@ end
 class Story < Struct.new(:headline, :domain, :url, :text) ; end
 
 File.open(File.expand_path(File.dirname(__FILE__)) + "/../public/index.html", "w") do |file|
-  feed = FeedNormalizer::FeedNormalizer.parse(File.read(File.expand_path(File.dirname(__FILE__)) + "/latest.rss"))
+  rss = File.expand_path(File.dirname(__FILE__)) + "/latest.rss"
+  feed = FeedNormalizer::FeedNormalizer.parse(File.read(rss))
   feed.clean!
 
   @stories = (feed.entries.collect do |entry|
@@ -62,10 +63,11 @@ File.open(File.expand_path(File.dirname(__FILE__)) + "/../public/index.html", "w
     title = entry.title
     comments_regex = /<a href="http:\/\/news\.ycombinator\.com\/item\?id=\d+">Comments<\/a>/
     comments_url = comments_regex.match(entry.content)
-    # now we have the comments_url. however, this is not featured anywhere in the output or the template. this is
-    # an intentional move for the sake of quality control. I'm considering adding a comments link, but what might
-    # be a lot better is to add a feature which scrapes the comments_url for comments from patio11, peterc, pg, maxklein,
-    # etc., so you can estimate at a glance whether the comments are worth reading or not
+    # now we have the comments_url. however, this is not featured anywhere in the output or
+    # the template. this is an intentional move for the sake of quality control. I'm considering
+    # adding a comments link, but what might be a lot better is to add a feature which scrapes
+    # the comments_url for comments from patio11, peterc, pg, maxklein, etc., so you can estimate
+    # at a glance whether the comments are worth reading or not
     entry.content.gsub!(comments_regex, "")
     text = Hpricot(entry.content).to_plain_text
     [text, title].each do |string|
@@ -115,5 +117,5 @@ end
 
 # sometimes this process fails, creating a blank page. unsure why currently;
 # here's a band-aid for it.
-booty_gremlins if File.new("/webapps/hacker_newspaper/public/index.html").stat.zero?
+error_page if File.new("/webapps/hacker_newspaper/public/index.html").stat.zero?
 
