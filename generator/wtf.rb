eaea3bdc98687b11937d8465daa4e5ea926f0b20
Giles Bowkett <gilesb@gmail.com>
2012-01-27 11:29:59 -0800

    better error-handling
    moved the file-generating block to happen after the parsing. this
    means that if we get fucked-up HTML/etc for some reason, we will
    at worst get an error page.

--- generator/wtf.rb
+++ generator/wtf.rb
@@ -52,66 +52,72 @@ end
 
 class Story < Struct.new(:headline, :domain, :url, :text) ; end
 
-File.open(File.expand_path(File.dirname(__FILE__)) + "/../public/index.html", "w") do |file|
-  rss = File.expand_path(File.dirname(__FILE__)) + "/latest.rss"
-  feed = FeedNormalizer::FeedNormalizer.parse(File.read(rss))
+rss = File.expand_path(File.dirname(__FILE__)) + "/latest.rss"
+feed = FeedNormalizer::FeedNormalizer.parse(File.read(rss))
+begin
   feed.clean!
+rescue Hpricot::ParseError
+  error_page
+end
 
-  @stories = (feed.entries.collect do |entry|
-    next unless entry.url && entry.title
+@stories = (feed.entries.collect do |entry|
+  next unless entry.url && entry.title
 
-    title = entry.title
-    comments_regex = /<a href="http:\/\/news\.ycombinator\.com\/item\?id=\d+">Comments<\/a>/
-    comments_url = comments_regex.match(entry.content)
-    # now we have the comments_url. however, this is not featured anywhere in the output or
-    # the template. this is an intentional move for the sake of quality control. I'm considering
-    # adding a comments link, but what might be a lot better is to add a feature which scrapes
-    # the comments_url for comments from patio11, peterc, pg, maxklein, etc., so you can estimate
-    # at a glance whether the comments are worth reading or not
-    entry.content.gsub!(comments_regex, "")
-    text = Hpricot(entry.content).to_plain_text
-    [text, title].each do |string|
-      string.new_york_times_get_over_your_pretentious_bullshit_please
-    end
-    text.gsub!(/^!\[CDATA\[http:\/\/news\.ycombinator\.com\/item\?id=\d+\]/, "")
-    text.gsub!(/\[[^\[]+\]/, "")
-    text = text[0..300]
+  title = entry.title
+  comments_regex = /<a href="http:\/\/news\.ycombinator\.com\/item\?id=\d+">Comments<\/a>/
+  comments_url = comments_regex.match(entry.content)
+  # now we have the comments_url. however, this is not featured anywhere in the output or
+  # the template. this is an intentional move for the sake of quality control. I'm considering
+  # adding a comments link, but what might be a lot better is to add a feature which scrapes
+  # the comments_url for comments from patio11, peterc, pg, maxklein, etc., so you can estimate
+  # at a glance whether the comments are worth reading or not
+  entry.content.gsub!(comments_regex, "")
+  text = Hpricot(entry.content).to_plain_text
+  [text, title].each do |string|
+    string.new_york_times_get_over_your_pretentious_bullshit_please
+  end
+  text.gsub!(/^!\[CDATA\[http:\/\/news\.ycombinator\.com\/item\?id=\d+\]/, "")
+  text.gsub!(/\[[^\[]+\]/, "")
+  text = text[0..300]
 
-    match = /http:\/\/([^\/]+)\//.match(entry.url)
-    match ? domain = match[1] : next
+  match = /http:\/\/([^\/]+)\//.match(entry.url)
+  match ? domain = match[1] : next
 
-    # I don't care what these people say
-    banned = %w{techcrunch
-                codinghorror
-                steve-yegge
-                marco.org
-                skorks
-                learnpythonthehardway
-                sheddingbikes
-                oppugn.us}.inject(false) do |memo, frequent_timewaster|
-      domain.include?(frequent_timewaster) ? true : memo
-    end
+  # I don't care what these people say
+  banned = %w{techcrunch
+              uncrunched
+              codinghorror
+              steve-yegge
+              marco.org
+              skorks
+              learnpythonthehardway
+              sheddingbikes
+              oppugn.us}.inject(false) do |memo, frequent_timewaster|
+    domain.include?(frequent_timewaster) ? true : memo
+  end
 
-    # I don't care what anybody says about these topics
-    banned = true if /zed shaw/i =~ text
-    banned = true if /zed shaw/i =~ title
+  # I don't care what anybody says about these topics
+  banned = true if /zed shaw/i =~ text
+  banned = true if /zed shaw/i =~ title
 
-    # banhammer of zillyhoo
-    next if banned
+  # banhammer of zillyhoo
+  next if banned
 
-    # avoiding a shit-ton of Unicode
-    text = "" if domain =~ /wikipedia/
+  # avoiding a shit-ton of Unicode
+  text = "" if domain =~ /wikipedia/
 
-    Story.new(title, domain, entry.url, text)
-  end).compact
-  
-  # ridiculous easter egg
-  images = []
-  Dir.foreach("/webapps/hacker_newspaper/public/images") {|image| images << image if image =~ /\.jpg/}
-  @image = images[rand(images.size)]
-  template = (rand > 0.97 ? "lolcats" : "template")
+  Story.new(title, domain, entry.url, text)
+end).compact
 
-  opened_template = File.read(File.expand_path(File.dirname(__FILE__)) + "/#{template}.erb")
+# ridiculous easter egg
+images = []
+Dir.foreach("/webapps/hacker_newspaper/public/images") {|image| images << image if image =~ /\.jpg/}
+@image = images[rand(images.size)]
+template = (rand > 0.97 ? "lolcats" : "template")
+
+opened_template = File.read(File.expand_path(File.dirname(__FILE__)) + "/#{template}.erb")
+
+File.open(File.expand_path(File.dirname(__FILE__)) + "/../public/index.html", "w") do |file|
   file.puts ERB.new(opened_template).result(binding)
 end
 
